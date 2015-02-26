aebe161d39626b19bb0f2e5260c92f3964a6b532
Bruce Cumming <bgc@lsr.nei.nih.gov>
2015-02-26 15:54:34 -0500

    added some files to matlab/extras

--- matlab/extras/GetDataFromFig.m
+++ matlab/extras/GetDataFromFig.m
@@ -2,18 +2,40 @@ function [DATA, fn] = GetDataFromFig(a, varargin)
 %if a subrouting has been called from a uicontrol, this find
 % UserData from the parent figure
 fn = 0;
+javaobj = 0;
     if isstruct(a) %can call a callback routine manually, passing DATA
         DATA = a;
         return;
     end
-        
-    DATA = get(a,'UserData');
 
+    if strncmp(class(a),'javahandle',8)
+        a = get(a,'uipanel');
+        javaobj = 1;
+    end
+    b = GetFigure(a,'noforce');
+    if isappdata(b,'ParentFigure')
+        b = getappdata(b,'ParentFigure');
+        DATA = get(b,'UserData');
+        return;
+    elseif javaobj
+        DATA = get(b, 'UserData');
+        return;
+    end
+    DATA = get(a,'UserData');
     b = a;
     while isempty(DATA) & ~isempty(b)
-        b = get(b,'parent');
         if isfigure(b)
             fn = b;
+            if isempty(DATA)
+                p = getappdata(b,'ParentFigure');
+                if ~isempty(p)
+                    DATA = get(p,'UserData');
+                    break;
+                end
+            end
+            b = get(b,'parent');
+        else
+            b = GetFigure(b,'noforce');
         end
         DATA = get(b,'UserData');
     end
@@ -21,6 +43,8 @@ fn = 0;
         DATA = get(DATA,'UserData');
     elseif isfield(DATA,'parentfig')
         DATA = get(DATA.parentfig,'UserData');
+    elseif isfield(DATA,'toplevel')
+        DATA = get(DATA.toplevel,'UserData');
     elseif isfield(DATA,'parentfigtag')
         DATA = get(findobj('Tag',DATA.parentfigtag,'Type','Figure'),'UserData');
     end
--- matlab/vergversion.m
+++ matlab/vergversion.m
@@ -1,3 +1,3 @@
 function r = vergversion()
-r = 'verg.279.fd19fcd';
+r = 'verg.280.3a5e15b';
 
